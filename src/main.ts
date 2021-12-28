874cc36ecd8206d0e5cf463dc431a08d2fadd823
José Silva <jose.silva@ciandt.com>
2021-12-28 16:48:22 -0300

    feat: express added

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,19 @@
-function main() {}
+import express, { Router } from 'express';
+
+function main() {
+  const app = express();
+  const router = Router();
+
+  app.use(express.json());
+  app.use(router);
+
+  router.get('/', (req, res) => {
+    res.status(200).json({ foo: 'bar' });
+  });
+
+  app.listen(3000, () => {
+    console.log("🚀 it's alive 🎉 (http://localhost:3000)");
+  });
+}
 
 main();
